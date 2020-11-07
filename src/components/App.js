8ea77c5e71de3852fc13603fc33c25236c0d37e1
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-07 22:50:51 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     return (
       <>
-        <h1 data-ns-test="project-name">Project Name</h1>
-        <p data-ns-test="project-description">Project Description</p>
+        <h1 data-ns-test="project-name">{`Project Name`}</h1>
+        <p data-ns-test="project-description">{`Project Description`}</p>
       </>
     );
   }
